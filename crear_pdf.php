583f32356df1b907b9016ab6127d2dabbf1115a8
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2018-12-19 13:55:49 -0300

    fix para que el salto de pagina no corte la tabla y width al 100% de la misma

--- crear_pdf.php
+++ crear_pdf.php
@@ -3,12 +3,14 @@
 include('vendor/autoload.php');
 
 $mpdf = new \Mpdf\Mpdf();
+$mpdf->shrink_tables_to_fit = 1;
+
 $simbolos = simplexml_load_file($_FILES['archivo']['tmp_name']);
 
 $html ="";
 
 foreach($simbolos as $simbolo){
-    $html.= '<table style="width:50%; border:1px solid black">';
+    $html.= '<table style="width:100%; border:1px solid black; page-break-inside: avoid">';
     
     $html.= '<tr><td style="width:100%; border:1px solid black; text-align: center"><h1>'. htmlentities($simbolo->nombreSimbolo). '</h1></td></tr>';
     $html.= '<tr><td style="width:100%; border:1px solid black; padding: 2%"><strong>Noci&oacute;n:</strong><br><ul>';
